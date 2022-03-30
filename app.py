95e50cb67295d5a353d906b5b392bc2b27a7dd61
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-03-30 19:42:01 -0400

    app.py for new flask

--- app.py
+++ app.py
@@ -1,75 +1,22 @@
-import datetime as dt
-import numpy as np
-import pandas as pd
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
-
-#set up the database engine
-engine = create_engine("sqlite:///hawaii.sqlite")
-
-#reflect the tables
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-#references to each table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-
-#create session link
-session = Session(engine)
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return(
-    '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
-    ''')
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= prev_year).all()
-   precip = {date: prcp for date, prcp in precipitation}
-   return jsonify(precip)
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
-@app.route("/api/v1.0/stations")
-def stations():
-    results = session.query(Station.station).all()
-    stations = list(np.ravel(results))
-    return jsonify(stations=stations)
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
-@app.route("/api/v1.0/tobs")
-def temp_monthly():
-    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    results = session.query(Measurement.tobs).\
-      filter(Measurement.station == 'USC00519281').\
-      filter(Measurement.date >= prev_year).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)
-
-@app.route("/api/v1.0/temp/<start>")
-@app.route("/api/v1.0/temp/<start>/<end>")
-def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-    
-    if not end:
-        results = session.query(*sel).\
-            filter(Measurement.date >= start).all()
-        temps = list(np.ravel(results))
-        return jsonify(temps=temps)
-    
-    results = session.query(*sel).\
-        filter(Measurement.date >= start).\
-        filter(Measurement.date <= end).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps)
